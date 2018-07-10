c8c4f488b667e52013b5b6adca9475216abcae5f
Tomas Cohen Arazi <tomascohen@theke.io>
2018-07-10 15:58:28 -0300

    Initial functionality commit
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ Koha/Plugin/Com/Theke/PaymentPIN.pm
@@ -0,0 +1,227 @@
+package Koha::Plugin::Com::Theke::PaymentPIN;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use Koha::Patrons;
+use Koha::Patron::Attribute::Types;
+use Koha::Patron::Attributes;
+
+use JSON;
+use List::MoreUtils qw(any);
+
+use Exception::Class (
+    'PaymentPIN::Exception' => {
+        fields => [ 'response' ]
+    }
+);
+
+our $VERSION = "{VERSION}";
+
+our $metadata = {
+    name            => 'Payment+PIN plugin',
+    author          => 'Tomas Cohen Arazi',
+    description     => 'Provides a way to validate PIN (as an extended patron attribute) and make payments',
+    date_authored   => '2018-07-05',
+    date_updated    => '2018-07-05',
+    minimum_version => '17.11',
+    maximum_version => undef,
+    version         => $VERSION,
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+sub tool {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    my $step = $cgi->param('step') // 'configure';
+    if ( $step eq 'configure' ) {
+        $self->configure_store;
+    }
+    elsif ( $step eq 'validate' ) {
+        $self->validate;
+    }
+    else {
+        # requested to pay
+        $self->pay;
+    }
+}
+
+sub validate {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{cgi};
+
+    my $cardnumber = $cgi->param('cardnumber');
+    my $pin        = $cgi->param('pin');
+
+    return try {
+        my $attribute_type = $self->retrieve_data('attribute_type');
+        unless ($attribute_type) {
+            PaymentPIN::Exception->throw(
+                {   response => {
+                        authorized => JSON::false,
+                        error      => 'Configuration error: missing pin attribute type'
+                    }
+                }
+            );
+        }
+
+        my $att_object = Koha::Patron::Attribute::Types->find($attribute_type);
+        unless ($att_object) {
+            PaymentPIN::Exception->throw(
+                {   response => {
+                        authorized => JSON::false,
+                        error      => 'Configuration error: non-existent attribute type'
+                    }
+                }
+            );
+        }
+
+        unless ($pin) {
+            return { authorized => JSON::false, error => 'PIN parameter missing' };
+        }
+
+        my $patron = Koha::Patrons->find( { cardnumber => $cardnumber } );
+        unless ($patron) {
+            return { authorized => JSON::false, error => 'Invalid cardnumber' };
+        }
+
+        my $attributes = Koha::Patron::Attributes->search(
+            { code => $attribute_type, borrowernumber => $patron->id } );
+
+        unless ( $attributes->count > 0 ) {
+            return { authorized => JSON::false };
+        }
+
+        if ( any { $_->attribute eq $pin } @{ $attributes->as_list } ) {
+
+            # PIN matches
+            return { authorized => JSON::true };
+        }
+        else {
+            return { authorized => JSON::false };
+        }
+    }
+    catch {
+        if ( $_->isa('PaymentPIN::Exception') ) {
+            $_->rethrow();
+        }
+    };
+
+
+
+}
+
+sub pay {
+    my ( $self, $args ) = @_;
+
+    my $cgi          = $self->{cgi};
+    my $payment_data = decode_json $cgi->param('POSTDATA');
+    my $cardnumber   = $payment_data->{cardnumber};
+
+    my $patron;
+    if ( $cardnumber ) {
+        $patron =  Koha::Patrons->find( { cardnumber => $cardnumber } );
+    }
+
+    unless ($patron) {
+       PaymentPIN::Exception->throw( { response => 
+           { authorized => JSON::false, error => "Invalid cardnumber" }
+       } );
+    }
+
+    $patron->account->pay(
+        {
+            amount => $amount
+        }
+    );
+
+    return { result => 1 };
+}
+
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{cgi};
+
+    my $template = $self->get_template({ file => 'configure.tt' });
+
+    my $api_key        = $self->retrieve_data( 'api_key' );
+    my $attribute_type = $self->retrieve_data( 'attribute_type' );
+
+    $template->param(
+        api_key        => $api_key,
+        attribute_type => $attribute_type
+    );
+
+    print $cgi->header( -charset => 'utf-8' );
+    print $template->output();
+}
+
+sub configure_store {
+    my ( $self, $args ) = @_;
+
+    my $cgi            = $self->{cgi};
+    my $api_key        = $cgi->param('api_key');
+    my $attribute_type = $cgi->param('attribute_type');
+
+    # Store new API key
+    $self->store_data({ 'api_key'        => $api_key });
+    $self->store_data({ 'attribute_type' => $attribute_type });
+
+    my $template = $self->get_template({ file => 'configure.tt' });
+
+    $template->param(
+        api_key        => $api_key,
+        attribute_type => $attribute_type
+    );
+
+    print $cgi->header( -charset => 'utf-8' );
+    print $template->output();
+
+}
+
+sub response {
+    my ( $self, $args ) = @_;
+
+    print $self->{cgi}->header(
+        -status  => $args->{status},
+        -charset => 'UTF-8',
+        -type    => 'application/json'
+    ), encode_json($args->{response});
+    return 1;
+}
+
+sub api_key_valid {
+    my ( $self, $api_key ) = @_;
+
+    my $ret;
+
+    if ( defined $api_key ) {
+        my $stored_api_key = $self->retrieve_data( 'api_key' );
+
+        if ( $stored_api_key and $api_key eq $stored_api_key ) {
+            $ret = 1;
+        }
+    }
+
+    return $ret;
+}
+
+sub uninstall {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,38 @@
+# Koha Payment+PIN plugin
+
+## Downloading
+
+From the [release page](https://github.com/thekesolutions/koha-plugin-payment-pin/releases) you can download the relevant *.kpz file
+
+# Installing
+
+Koha's Plugin System allows for you to add additional tools and reports to Koha that are specific to your library. Plugins are installed by uploading KPZ ( Koha Plugin Zip ) packages. A KPZ file is just a zip file containing the perl files, template files, and any other files necessary to make the plugin work.
+
+The plugin system needs to be turned on by a system administrator.
+
+To set up the Koha plugin system you must first make some changes to your install.
+
+* Change `<enable_plugins>0<enable_plugins>` to `<enable_plugins>1</enable_plugins>` in your koha-conf.xml file
+* Confirm that the path to `<pluginsdir>` exists, is correct, and is writable by the web server
+* Restart your webserver
+
+You need to tweak your _Apache_ vhost configuration for the intranet. If you are using the packages
+install method (you should!) given the instance name **instance** you need to edit the
+_/etc/apache2/sites-available/**instance**.conf_ file. Look for the intranet vhost and add this:
+
+```
+ScriptAlias /pin_validator.pl "/var/lib/koha/instance/plugins/Koha/Plugin/Com/Theke/PaymentPIN/pin_validator.pl"
+ScriptAlias /payment.pl "/var/lib/koha/instance/plugins/Koha/Plugin/Com/Theke/PaymentPIN/payment.pl"
+Alias /plugin "/var/lib/koha/instance/plugins"
+<Directory /var/lib/koha/instance/plugins>
+      Options Indexes FollowSymLinks
+      AllowOverride None
+      Require all granted
+</Directory>
+```
+
+Then restart _apache_:
+```
+$ sudo systemctl restart apache2.service
+
+Once set up is complete you will need to alter your UseKohaPlugins system preference. On the Tools page you will see the Tools Plugins and on the Reports page you will see the Reports Plugins.
